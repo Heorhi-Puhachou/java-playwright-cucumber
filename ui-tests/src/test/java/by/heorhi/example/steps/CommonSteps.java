fc00998f2e65eac23d8acdab36e14eae9a075c77
Heorhi Puhachou <ce-heorhi.puhachou@beyondnow.com>
2023-06-23 12:56:38 +0200

    Test update

--- ui-tests/src/test/java/by/heorhi/example/steps/CommonSteps.java
+++ ui-tests/src/test/java/by/heorhi/example/steps/CommonSteps.java
@@ -4,6 +4,7 @@ import by.heorhi.example.context.UIContext;
 import by.heorhi.example.utils.PageUtils;
 import com.microsoft.playwright.Page;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 
 public class CommonSteps {
 
@@ -14,5 +15,11 @@ public class CommonSteps {
     @And("wait loader is not shown")
     public void waitLoaderIsNotShown() {
         PageUtils.waitForElementNotThere(getPage(), "div.preloader");
+    }
+
+    @Then("text {string} is visible")
+    public void checkTextVisible(String text) {
+        String textElementLocator = "text=" + text;
+        getPage().waitForSelector(textElementLocator);
     }
 }
--- ui-tests/src/test/resources/features/firstPage.feature
+++ ui-tests/src/test/resources/features/firstPage.feature
@@ -1,8 +1,7 @@
 Feature: First page
 
   @browser
-  Scenario: Check all elements on first page
+  Scenario: Check elements on first page
     When load first page
     And wait loader is not shown
-    When all step definitions are implemented
-    Then the scenario passes
+    Then text "First page" is visible

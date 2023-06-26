9cf20f2c44012968d9b919bb90c7496c2b0f8e09
Heorhi Puhachou <ce-heorhi.puhachou@beyondnow.com>
2023-06-26 11:51:24 +0200

    Check list composite values

--- ui-tests/src/test/java/by/heorhi/example/pages/ThirdPage.java
+++ ui-tests/src/test/java/by/heorhi/example/pages/ThirdPage.java
@@ -1,12 +1,29 @@
 package by.heorhi.example.pages;
 
+import com.microsoft.playwright.ElementHandle;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 public class ThirdPage extends AbstractPage {
 
     public void openPage() {
         this.navigateFromBaseUrl("thirdPage");
     }
 
-    public Integer getInputsCount() {
-        return getPage().querySelectorAll("input").size();
+    public List<ElementHandle> getListsByName(String listName) {
+        return getPage().querySelectorAll("div[test-locator='list-name'] >> .. >> ul");
+    }
+
+    public void checkListElements(ElementHandle listToCheck, List<Map<String, String>> rows) {
+        listToCheck.querySelectorAll("li").size();
+        for (Map<String, String> row : rows) {
+            String position = row.get("position");
+            String value = row.get("value");
+            List<ElementHandle> elementsForRow = listToCheck.querySelectorAll("li >> div[test-locator='position'] >> text=" + position + ">> .. >>div[test-locator='value'] >> text=" + value);
+            assertEquals(1, elementsForRow.size());
+        }
     }
 }
--- ui-tests/src/test/java/by/heorhi/example/steps/ThirdPageSteps.java
+++ ui-tests/src/test/java/by/heorhi/example/steps/ThirdPageSteps.java
@@ -1,13 +1,11 @@
 package by.heorhi.example.steps;
 
-
+import by.heorhi.example.pages.ThirdPage;
 import com.microsoft.playwright.ElementHandle;
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
@@ -24,8 +22,10 @@ public class ThirdPageSteps {
 
     @Then("list {string} contain records")
     public void checkPackageQuantity(String listName, DataTable dataTable) {
-        ElementHandle tableElement = taskPage.checkTableOnPage(tableName);
+        List<ElementHandle> listsByName = thirdPage.getListsByName(listName);
+        assertEquals(1, listsByName.size());
+        ElementHandle listToCheck = listsByName.get(0);
         List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
-        thirdPage.checkMaterialRows(tableElement, rows));
+        thirdPage.checkListElements(listToCheck, rows);
     }
 }
